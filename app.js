d93f14ea77388c20a4ab53b184e4b184129d1a92
Joseph Wong-Ludwiczak <josclud@gmail.com>
2021-11-03 23:03:49 -0700

    added js

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-let listItem = document.createElement('li')
-listItem.innerHTML = '<li class="list-group-item"></li>'
+document.getElementById('addItem').addEventListener('click', event => {
+  event.preventDefault()
+  
+  let itemElem = document.createElement('li')
+  itemElem.className = 'list-group-item'
+  itemElem.textContent = document.getElementById('item').value 
 
-document.getElementById('list').append(listItem)
-
-document.getElementById('button').addEventListener('click', () => {
-  event.preventDefault();
-  document.getElementById('listItem').textContent = document.getElementById('form').value
+  document.getElementById('items').append(itemElem)
+  document.getElementById('item').value = ''
 })
+
+// const listItem = () => {
+//   let li = document.createElement('list');
+//   list.appendChild(li)
+// }
+
+// // let listItem = document.createElement('li')
+// // listItem.innerHTML = '<li class="list-group-item"></li>'
+
+
+// document.getElementById('button').addEventListener('click', () => {
+//   event.preventDefault();
+//   document.createElement(listItem()).textContent = document.getElementById('form').value
+// })
+
+
+// document.getElementById('button').addEventListener('click', () => {
+//   event.preventDefault();
+//   document.getElementById('listItem').textContent = document.getElementById('exampleInputEmail1').value;
+//   document.getElementById('form').value = ''
+// })
