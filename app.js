252056315acf2cb6deda0d780835c67e11edbf01
Joseph Wong-Ludwiczak <josclud@gmail.com>
2021-11-11 13:40:49 -0800

    added toggle for completed items

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 document.getElementById('addItem').addEventListener('click', event => {
   event.preventDefault()
 
-  let itemElem = document.createElement('li')
+  const itemElem = document.createElement('li')
   itemElem.className = 'list-group-item d-flex justify-content-between align-items-left'
   itemElem.innerHTML = `
   ${document.getElementById('item').value}
+  <div>
+  <button class="btn btn-success done">✓</button>
   <button class="btn btn-danger delete">X</button>
+  </div>
   `
   document.getElementById('items').append(itemElem)
   document.getElementById('item').value = ''
 })
 
+// document.addEventListener('click', event => {
+//   if (event.target.classList.contains('done')) {
+//     document.querySelector('unit').style.color = 'green'
+//   }
+// })
+
 document.addEventListener('click', event => {
   if (event.target.classList.contains('delete')) {
-    event.target.parentNode.remove()
+    event.target.parentNode.parentNode.remove()
+  } else if (event.target.classList.contains('done')) {
+    if (event.target.parentNode.parentNode.style.color === 'green') {
+      event.target.parentNode.parentNode.style.color = 'black'
+    } else {
+      event.target.parentNode.parentNode.style.color = 'green'
+    }
   }
 })
 
